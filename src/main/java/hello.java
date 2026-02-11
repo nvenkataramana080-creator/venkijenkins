4a13c523f08fb233e6ed7b3810795634a376fec7
N VENKATARAMANA <nvenkataramana080@gmail.com>
2026-02-11 23:12:47 +0530

    Remove redundant print statement

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,5 @@ package main.java;
 public class hello{
      public static void main(String []args){
         System.out.println("hai visionary venki");
-         System.out.println("hai visionary kdnckn"); 
      }
     }
