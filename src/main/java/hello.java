7c498c78452b0557ba161fa7612d42bba5904fae
N VENKATARAMANA <nvenkataramana080@gmail.com>
2026-02-11 23:08:13 +0530

    Add greeting message for visionary kdnckn

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,6 @@ package main.java;
 public class hello{
      public static void main(String []args){
         System.out.println("hai visionary venki");
+         System.out.println("hai visionary kdnckn"); 
      }
     }
